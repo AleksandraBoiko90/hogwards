9d4bf148f4dcf9cf80ac62cc215e42430b68711b
alexandra.boiko <AleksandraBoiko90@gmail.com>
2024-10-06 13:28:12 +0300

    change student card

--- script.js
+++ script.js
@@ -39,25 +39,43 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     function createStudentCard(s) {
+
         const card = document.createElement('div');
         card.classList.add('student-card');
         // card.style.backgroundColor = getFacultyColor(s.faculty);
 
-        
+        addName();
+        addImg();
 
 
-        card.innerHTML = `
-        <img src="${s.imageUrl}" alt="${s.name}" style="height:100px;">
-        <h3>${s.name}</h3>
-        <p>Faculty: ${s.faculty}</p>
+        // card.innerHTML = `
+        // <img src="${s.imageUrl}" alt="${s.name}" style="height:100px;">
+        // <h3>${s.name}</h3>
+        // <p>Faculty: ${s.faculty}</p>
         
-        <button class="save-button" >Save</button>
-        <button class="delete-button" >Delete</button>
-        <button class="edit-button" >Edit</button>
-        `;
+        
+        // <button class="save-button" >Save</button>
+        // <button class="delete-button" >Delete</button>
+        // <button class="edit-button" >Edit</button>
+        // `;
 
         return card;
+
+
+        function addName() {
+            const tag = document.createElement('h3');
+            tag.innerText = s.name;
+            card.appendChild(tag);
+        }
+        function addImg() {
+            const tag = document.createElement('img');
+            tag.setAttribute('src', s.imageUrl);
+            tag.setAttribute('alt', s.name+ " photo");
+            card.appendChild(tag);
+        }
     }
+
+
 
 
 
