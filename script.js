642f9b0071675588c1223c42eab73be74e2d7918
alexandra.boiko <AleksandraBoiko90@gmail.com>
2024-10-06 13:14:46 +0300

    display student cards

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetchStudents();
+    fetchStudents().then(students => {
+        displayStudents(students);
 
 
+    });
 
     function fetchStudents() {
         let url = 'https://hp-api.onrender.com/api/characters/students';
 
-        fetch(url)
+        return fetch(url)
             .then(response => response.json())
             .then(data => {
                 const currentYear = (new Date()).getFullYear();
-                const apiStudents = data.map(s => {
+                const students = data.map(s => {
                     return {
+                        id: s.id,
                         name: s.name,
                         faculty: s.house,
                         imageUrl: s.image,
@@ -20,9 +23,42 @@ document.addEventListener('DOMContentLoaded', function () {
                         wand: s.wand
                     };
                 });
-
-                //todo: displayStudents(apiStudents);
+                return students;
             });
 
     }
-});
+
+    function displayStudents(students) {
+
+        const container = document.querySelector('.students-container');
+        students.forEach(s => {
+            container.appendChild(createStudentCard(s));
+        });
+
+    }
+
+
+    function createStudentCard(s) {
+        const card = document.createElement('div');
+        card.classList.add('student-card');
+        // card.style.backgroundColor = getFacultyColor(s.faculty);
+
+        
+
+
+        card.innerHTML = `
+        <img src="${s.imageUrl}" alt="${s.name}" style="height:100px;">
+        <h3>${s.name}</h3>
+        <p>Faculty: ${s.faculty}</p>
+        
+        <button class="save-button" >Save</button>
+        <button class="delete-button" >Delete</button>
+        <button class="edit-button" >Edit</button>
+        `;
+
+        return card;
+    }
+
+
+
+});//document.addEventListener('DOMContentLoaded', function () {
