b65145b4e2c1c069bd51f664f4b914191a4806bb
alexandra.boiko <AleksandraBoiko90@gmail.com>
2024-10-06 12:19:27 +0300

    fetch students

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//todo: implement
+document.addEventListener('DOMContentLoaded', function () {
+    fetchStudents();
+
+
+
+    function fetchStudents() {
+        let url = 'https://hp-api.onrender.com/api/characters/students';
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const currentYear = (new Date()).getFullYear();
+                const apiStudents = data.map(s => {
+                    return {
+                        name: s.name,
+                        faculty: s.house,
+                        imageUrl: s.image,
+                        alternate_names: s.alternate_names,
+                        age: currentYear - s.yearOfBirth,
+                        wand: s.wand
+                    };
+                });
+
+                //todo: displayStudents(apiStudents);
+            });
+
+    }
+});
